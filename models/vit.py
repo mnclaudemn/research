307d5ab9ea732cc2faf532f1e0e06014646c9a33
M M <mnclaudemn@gmail.com>
2026-06-22 07:04:27 -0700

    Update vit.py

--- models/vit.py
+++ models/vit.py
@@ -1,1 +1,9 @@
+def vit_base(num_classes, pretrained=True):
 
+    model = timm.create_model(
+        "vit_base_patch16_224",
+        pretrained=pretrained,
+        num_classes=num_classes
+    )
+
+    return model
