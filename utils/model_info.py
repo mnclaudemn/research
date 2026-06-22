4b14a1a140a626c29f976ade6bd0df491ca5cf45
M M <mnclaudemn@gmail.com>
2026-06-22 07:18:33 -0700

    Update model_info.py

--- utils/model_info.py
+++ utils/model_info.py
@@ -1,1 +1,8 @@
 
+def print_trainable_layers(model):
+
+    print("\nTrainable parameters:\n")
+
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            print(name)
