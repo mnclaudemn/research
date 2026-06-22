9cd2724ce0cc782db77cd421ecc25c771fc898d2
M M <mnclaudemn@gmail.com>
2026-06-22 07:06:48 -0700

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,1 +1,23 @@
 
+def evaluate(model, loader, criterion, device):
+
+    model.eval()
+    total_loss = 0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for x, y in loader:
+            x, y = x.to(device), y.to(device)
+
+            preds = model(x)
+            loss = criterion(preds, y)
+
+            total_loss += loss.item()
+
+            _, predicted = preds.max(1)
+            correct += predicted.eq(y).sum().item()
+            total += y.size(0)
+
+    acc = 100. * correct / total
+    return total_loss / len(loader), acc
