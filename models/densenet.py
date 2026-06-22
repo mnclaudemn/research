be1e9d7c6d4fb5d27029c9febdb9fc5724ce48c5
M M <mnclaudemn@gmail.com>
2026-06-22 07:04:11 -0700

    Add densenet121 function for model creation

--- models/densenet.py
+++ models/densenet.py
@@ -1,1 +1,9 @@
+def densenet121(num_classes, pretrained=True):
 
+    weights = models.DenseNet121_Weights.IMAGENET1K_V1 if pretrained else None
+
+    model = models.densenet121(weights=weights)
+
+    model.classifier = nn.Linear(model.classifier.in_features, num_classes)
+
+    return model
