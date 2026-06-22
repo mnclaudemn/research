01699525011cb035f209a2a72b0f5b3a88416a92
M M <mnclaudemn@gmail.com>
2026-06-22 08:42:40 -0700

    Update dino_wrapper.py

--- models/dino_wrapper.py
+++ models/dino_wrapper.py
@@ -1,10 +1,29 @@
 import torch
+import torch.nn as nn
 
-def dino_vit():
 
-    model = torch.hub.load(
-        'facebookresearch/dino:main',
-        'dino_vits16'
-    )
+class DinoClassifier(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
 
-    return model
+        self.backbone = torch.hub.load(
+            'facebookresearch/dino:main',
+            'dino_vits16'
+        )
+
+        self.head = nn.Linear(
+            self.backbone.embed_dim,
+            num_classes
+        )
+
+    def forward(self, x):
+
+        features = self.backbone(x)
+
+        logits = self.head(features)
+
+        return logits
+
+
+def dino_vit(num_classes):
+    return DinoClassifier(num_classes)
