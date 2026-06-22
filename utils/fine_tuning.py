699043b33bccdb56eb3d8316377ba54a03bf8dc4
M M <mnclaudemn@gmail.com>
2026-06-22 07:17:50 -0700

    Update fine_tuning.py

--- utils/fine_tuning.py
+++ utils/fine_tuning.py
@@ -1,1 +1,23 @@
 
+def freeze_model(model):
+    """
+    Freeze all parameters.
+    """
+    for param in model.parameters():
+        param.requires_grad = False
+
+
+def unfreeze_layers(model, layer_names):
+    """
+    Unfreeze only selected layers.
+    """
+
+    if "all" in layer_names:
+        for param in model.parameters():
+            param.requires_grad = True
+        return
+
+    for name, module in model.named_modules():
+        if name in layer_names:
+            for param in module.parameters():
+                param.requires_grad = True
