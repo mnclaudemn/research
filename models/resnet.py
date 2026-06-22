a2d6e5562602a6fb65a8fe00142e5e21ede5b727
M M <mnclaudemn@gmail.com>
2026-06-22 07:03:56 -0700

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -1,1 +1,9 @@
+def resnet50(num_classes, pretrained=True):
 
+    weights = models.ResNet50_Weights.IMAGENET1K_V2 if pretrained else None
+
+    model = models.resnet50(weights=weights)
+
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+
+    return model
