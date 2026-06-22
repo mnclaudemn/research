bd5d1650f61a13b2ef6252eaa4893f8c3c3f8110
M M <mnclaudemn@gmail.com>
2026-06-22 07:06:10 -0700

    Update engine.py

--- training/engine.py
+++ training/engine.py
@@ -1,1 +1,21 @@
 
+import torch
+
+def train_one_epoch(model, loader, optimizer, criterion, device):
+
+    model.train()
+    total_loss = 0
+
+    for x, y in loader:
+        x, y = x.to(device), y.to(device)
+
+        preds = model(x)
+        loss = criterion(preds, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    return total_loss / len(loader)
