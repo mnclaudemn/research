da86b6698e30b3bb314cf0f32a5b89811b5e1ae8
M M <mnclaudemn@gmail.com>
2026-06-22 07:05:10 -0700

    Update backbones.py

--- models/backbones.py
+++ models/backbones.py
@@ -1,3 +1,23 @@
 import torchvision.models as models
 import torch.nn as nn
 import timm
+
+def get_model(name, num_classes):
+
+    name = name.lower()
+
+    if name == "resnet50":
+        return resnet50(num_classes)
+
+    if name == "densenet121":
+        return densenet121(num_classes)
+
+    if name == "vit":
+        return vit_base(num_classes)
+
+    if name == "dino":
+        model = dino_vit()
+        model.head = nn.Linear(model.embed_dim, num_classes)
+        return model
+
+    raise ValueError("Unknown model")
