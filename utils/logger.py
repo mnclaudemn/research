d4fa7625b3e7778307680cad1f807f36384088d2
M M <mnclaudemn@gmail.com>
2026-06-22 09:58:27 -0700

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import os
 
+
+def save_result(path, result):
+
+    file_exists = os.path.isfile(path)
+
+    df = pd.DataFrame([result])
+
+    df.to_csv(path, mode="a", header=not file_exists, index=False)
