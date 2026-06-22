a113d53ecc651745caabb5782d030091dcff9eeb
M M <mnclaudemn@gmail.com>
2026-06-22 07:03:07 -0700

    Update medical_dataset.py

--- datasets/medical_dataset.py
+++ datasets/medical_dataset.py
@@ -1,1 +1,35 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
+def get_transforms():
+
+    train_tf = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
+
+    test_tf = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
+
+    return train_tf, test_tf
+
+
+def get_loaders(train_dir, test_dir, batch_size=16):
+
+    train_tf, test_tf = get_transforms()
+
+    train_ds = datasets.ImageFolder(train_dir, transform=train_tf)
+    test_ds = datasets.ImageFolder(test_dir, transform=test_tf)
+
+    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader, len(train_ds.classes)
