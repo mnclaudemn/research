6a1c77438ebf2174a89d763cd3071120ea230f5e
M M <mnclaudemn@gmail.com>
2026-06-22 10:14:14 -0700

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,12 +1,5 @@
 import numpy as np
-from sklearn.metrics import (
-    accuracy_score,
-    precision_score,
-    recall_score,
-    f1_score,
-    confusion_matrix,
-    roc_auc_score
-)
+from sklearn.metrics import (accuracy_score,precision_score,recall_score,f1_score, confusion_matrix,roc_auc_score)
 
 
 def compute_metrics(y_true, y_pred, y_prob=None, average="binary"):
