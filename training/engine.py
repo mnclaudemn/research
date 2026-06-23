f45c767e02381895625ff5a376ab57131b8a0036
M M <mnclaudemn@gmail.com>
2026-06-23 00:46:02 -0700

    Update engine.py

--- training/engine.py
+++ training/engine.py
@@ -1,20 +1,33 @@
-
 import torch
 
-def train_one_epoch(model, loader, optimizer, criterion, device):
+def train_one_epoch(model, loader, optimizer, criterion, device, scaler=None):
 
     model.train()
     total_loss = 0
 
     for x, y in loader:
-        x, y = x.to(device), y.to(device)
-
-        preds = model(x)
-        loss = criterion(preds, y)
+        x = x.to(device, non_blocking=True)
+        y = y.to(device, non_blocking=True)
 
         optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+
+        if scaler is not None:
+            # Mixed Precision
+            with torch.amp.autocast("cuda"):
+                preds = model(x)
+                loss = criterion(preds, y)
+
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+        else:
+            # CPU or no AMP
+            preds = model(x)
+            loss = criterion(preds, y)
+
+            loss.backward()
+            optimizer.step()
 
         total_loss += loss.item()
 
